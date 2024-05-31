3bdbffdf463281ee175d4d787a60118ef059ffbe
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-06-01 05:46:42 +0900

    Update

--- constants.py
+++ constants.py
@@ -6,8 +6,8 @@ SIM_TASK_CONFIGS = {
     'sim_transfer_cube_scripted':{
         'dataset_dir': DATA_DIR + '/sim_transfer_cube_scripted',
         'num_episodes': 50,
-        'episode_len': 400,
-        'camera_names': ['top']
+        'episode_len': 600,
+        'camera_names': ['main_camera']
     },
 
     'sim_transfer_cube_human':{
